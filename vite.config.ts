498e3bd427ed51319ab16a520cedfb5eb02e7c5f
DanielSanrui <daniel.sanc.ruiz@gmail.com>
2025-03-06 18:28:51 +0100

    commmmit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lol-generator/",
+  base: '/lol-generator/',
+  build: {
+    outDir: 'docs',
+  },
 })
